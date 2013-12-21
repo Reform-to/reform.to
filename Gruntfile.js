239b51b15142d5e05ff7c5225b70d522b6362659
Christopher Adams <chris@raysend.com>
2013-12-21 15:22:38 +0800

    Use vendor instead of bower_components

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 
     sass: {
       options: {
-        includePaths: ['bower_components/foundation/scss']
+        includePaths: ['vendor/foundation/scss']
       },
       dist: {
         options: {
