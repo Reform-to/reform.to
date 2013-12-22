85df414a8ed5a95723e43f83ee9cc34743d392d7
Christopher Adams <chris@raysend.com>
2013-12-22 16:00:53 +0800

    Put address form in its own component

--- js/congress.js
+++ js/congress.js
@@ -6,15 +6,23 @@ var CongressPicker = React.createClass({
     return (
     <div className="row">
       <div className="large-12 columns">
-        <form>
-          <fieldset>
-            <legend>Find Your Legislator</legend>
-            <input type="text" className="helper" placeholder="Enter an address to find your legislators"/>
-            <small className="helper">Start the search...</small>
-          </fieldset>
-        </form>
+        <AddressForm />
       </div>
     </div>
+    );
+  }
+});
+
+var AddressForm = React.createClass({
+  render: function() {
+    return (
+    <form>
+      <fieldset>
+        <legend>Find Your Legislator</legend>
+        <input type="text" className="helper" placeholder="Enter an address to find your legislators"/>
+        <small className="helper">Start the search...</small>
+      </fieldset>
+    </form>
     );
   }
 });
