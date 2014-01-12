ff7d3cf6a774c8ab6ae3d126823cd185d83c0ace
Christopher Adams <chris@raysend.com>
2014-01-12 09:55:28 +0800

    Lowercase the headline

--- app/jsx/candidates.jsx
+++ app/jsx/candidates.jsx
@@ -78,7 +78,9 @@ var CandidateLocator = React.createClass({
     <div className="ac-candidate-locator">
     <div className="row">
       <div className="large-12 columns">
-        <h2 className="subheader special-header text-center">Which reforms does your candidate support?</h2>
+        <h2 className="subheader special-header text-center text-lowercase">
+          Which reforms does your candidate support?
+        </h2>
         <AddressForm onAddressGeocode={this.locateCandidates} />
       </div>
     </div>
